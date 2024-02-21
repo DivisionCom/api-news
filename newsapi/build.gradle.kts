1a95aab4f8b6016936c5f65676e92c325381041f
DivisionCom <xd6511@gmail.com>
2024-02-21 14:36:09 +0300

    Add Libraries & Plugins
    Add Retrofit, Coroutines, Serialization

--- newsapi/build.gradle.kts
+++ newsapi/build.gradle.kts
@@ -1,9 +1,16 @@
 plugins {
     id("java-library")
     alias(libs.plugins.jetbrainsKotlinJvm)
+    alias(libs.plugins.kotlinxSerialization)
 }
 
 java {
     sourceCompatibility = JavaVersion.VERSION_1_7
     targetCompatibility = JavaVersion.VERSION_1_7
+}
+
+dependencies {
+    implementation(libs.retrofit)
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.serialization.json)
 }
