5f5d5c6e1b5aa86686344267059c80442f47a716
DivisionCom <xd6511@gmail.com>
2024-03-12 21:55:09 +0300

    Add 'news-data' Module

--- news-data/build.gradle.kts
+++ news-data/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 android {
-    namespace = "dev.divisioncom.news.main"
+    namespace = "dev.divisioncom.news.data"
     compileSdk = 34
 
     defaultConfig {
@@ -30,28 +30,12 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
-    buildFeatures {
-        compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.9"
-    }
 }
 
 dependencies {
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
-
-    implementation(libs.androidx.lifecycle.viewmodel.ktx)
-    implementation(libs.androidx.lifecycle.viewmodel.compose)
-
     implementation(libs.kotlinx.coroutines.android)
+
+    implementation(project(":database"))
+    implementation(project(":newsapi"))
 }
