90fee7f040394977e7e0d54a0e473bb835d874c6
DivisionCom <xd6511@gmail.com>
2024-03-20 11:32:25 +0300

    Add and Refactor Flow DB & API request

--- news-data/src/main/java/dev/divisioncom/news/data/ArticlesRepository.kt
+++ news-data/src/main/java/dev/divisioncom/news/data/ArticlesRepository.kt
@@ -2,21 +2,63 @@ package dev.divisioncom.news.data
 
 import dev.divisioncom.news.data.model.Article
 import dev.divisioncom.news.database.NewsDatabase
+import dev.divisioncom.news.database.models.ArticleDBO
 import dev.divisioncom.newsapi.NewsApi
+import dev.divisioncom.newsapi.models.ArticleDTO
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.filterIsInstance
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.mapNotNull
+import kotlinx.coroutines.flow.onEach
+import java.io.IOException
 
 class ArticlesRepository(
     private val database: NewsDatabase,
     private val api: NewsApi,
 ) {
 
-    fun getAll(): RequestResult<Flow<List<Article>>> {
-        return RequestResult.InProgress(
-            database.articlesDao
-                .getAll()
-                .map { articles -> articles.map { it.toArticle() } }
-        )
+    fun getAll(): Flow<RequestResult<List<Article>>> {
+        val cachedAllArticles: Flow<RequestResult.Success<List<ArticleDBO>>> = getAllFromDatabase()
+
+        val remoteArticles: Flow<RequestResult.Success<*>> = getAllFromServer()
+
+        cachedAllArticles.map
+        {
+
+        }
+        return cachedAllArticles.combine(remoteArticles)
+        {
+
+        }
+    }
+
+    private fun getAllFromServer(): Flow<RequestResult.Success<List<ArticleDBO>?>> {
+        return flow { emit(api.everything()) }
+            .map { result ->
+                if (result.isSuccess) {
+                    val response = result.getOrThrow()
+                    RequestResult.Success(response.articles)
+                } else {
+                    RequestResult.Error(null)
+                }
+            }
+            .filterIsInstance<RequestResult.Success<List<ArticleDTO>?>>()
+            .map { requestResult: RequestResult.Success<List<ArticleDTO>?> ->
+                requestResult.requireData()
+                    .map { articleDto -> articleDto.toArticleDbo() }
+                    .let { RequestResult.Success<List<ArticleDBO>?>(it) }
+            }.onEach { requestResult ->
+                database.articlesDao.insert(requestResult.requireData())
+            }
+    }
+
+    private fun getAllFromDatabase(): Flow<RequestResult.Success<List<ArticleDBO>>> {
+        return database.articlesDao
+            .getAll()
+            .map { RequestResult.Success(it) }
     }
 
     suspend fun search(query: String): Flow<Article> {
@@ -25,9 +67,11 @@ class ArticlesRepository(
     }
 }
 
-sealed class RequestResult<E>(protected val data: E?) {
+sealed class RequestResult<E>(internal val data: E?) {
 
     class InProgress<E>(data: E?) : RequestResult<E>(data)
     class Success<E>(data: E?) : RequestResult<E>(data)
     class Error<E>(data: E?) : RequestResult<E>(data)
 }
+
+internal fun <T : Any> RequestResult<T?>.requireData(): T = checkNotNull(data)
--- news-data/src/main/java/dev/divisioncom/news/data/Mappers.kt
+++ news-data/src/main/java/dev/divisioncom/news/data/Mappers.kt
@@ -2,7 +2,12 @@ package dev.divisioncom.news.data
 
 import dev.divisioncom.news.data.model.Article
 import dev.divisioncom.news.database.models.ArticleDBO
+import dev.divisioncom.newsapi.models.ArticleDTO
 
 internal fun ArticleDBO.toArticle() : Article {
     TODO("Not implemented")
+}
+
+internal fun ArticleDTO.toArticleDbo() : ArticleDBO {
+    TODO("Not yet implemented")
 }
